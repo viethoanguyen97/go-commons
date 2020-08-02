61ebd87208004d89762121f8d65bad385849cdf4
Viet Hoa Nguyen <viethoanguyen97@gmail.com>
2020-08-02 13:21:35 +0700

    GOC-1 minor fix

--- go-basic/Basics/More types: Structs, Slices and Maps/slices-literals.go
+++ go-basic/Basics/More types: Structs, Slices and Maps/slices-literals.go
@@ -6,19 +6,19 @@ func main() {
 	q := []int{2, 3, 5, 7, 11, 13}
 	fmt.Println(q)
 
-	r := []bool {true, false, true, true, false, true}
+	r := []bool{true, false, true, true, false, true}
 	fmt.Println(r)
 
 	s := []struct {
 		i int
-		b bool 
+		b bool
 	}{
-		{2, true}
-		{3, false}
-		{5, true}
-		{7, true}
-		{11, false}
-		{13, true}
+		{2, true},
+		{3, false},
+		{5, true},
+		{7, true},
+		{11, false},
+		{13, true},
 	}
 
 	fmt.Println(s)
